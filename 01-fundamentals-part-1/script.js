fc91063f1de49b3e4e46d680a4716b751050f828
Isa-Muhammad <isamuhammadm.im@gmail.com>
2025-07-14 15:22:45 +0100

    Values & Variables

---
+++ 01-fundamentals-part-1/script.js
@@ -0,0 +1,19 @@
+/* Topic: Values & Variables */
+
+// Values
+// A value is basically a piece of data, its the most fundamental unit
+// of information in programming.
+
+// let firstName = "Isa";
+// console.log(firstName);
+
+// // //Variable name conventions
+// let person = "Isa";
+// let PI = 3.1415; // All caps are for constants
+
+// // //writing descriptive variables
+
+// let myFirstJob = "programmer";
+// let myCurrentJob = "Front end dev";
+
+// console.log(myFirstJob);

a9fecff74e04dee5daf5b670a69721e30235f104
Isa-Muhammad <isamuhammadm.im@gmail.com>
2025-07-17 15:51:08 +0100

    Basic operators

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -43,16 +43,46 @@
 
 /* Topic: let, const and var */
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1998;
-// birthYear = 1999; // error
-// const job; // error
+// const birthYear = 1998;
+// // birthYear = 1999; // error
+// // const job; // error
 
-var job = "programmer";
-job = "teacher";
+// var job = "programmer";
+// job = "teacher";
 
-lastName = "Smith";
-console.log(lastName);
+// lastName = "Smith";
+// console.log(lastName);
 // //property was created on the global object not a variable in the current scope(bad practice). Using var also causes this behaviour
+
+/* Topic: Basic Operators */
+
+// Math operators
+const now = 2037;
+const ageIsa = now - 1998;
+const ageSarah = now - 2018;
+console.log(ageIsa, ageSarah);
+console.log(ageIsa * 2, ageIsa / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Isa";
+const lastName = "Smith";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+console.log(ageIsa > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1998 > now - 2018);

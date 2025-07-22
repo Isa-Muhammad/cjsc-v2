9682f94d06c0ee360850c5bc0bb04511eca86f08
Isa-Muhammad <isamuhammadm.im@gmail.com>
2025-07-22 15:53:15 +0100

    Taking Decisions: if / else Statements

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -134,21 +134,41 @@
 
 /* Topic: Strings and Template Literals */
 
-const firstName = "Isa";
-const job = "front end dev";
-const birthYear = 1998;
-const year = 2026;
+// const firstName = "Isa";
+// const job = "front end dev";
+// const birthYear = 1998;
+// const year = 2026;
 
-// const isa =
-//   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
-// console.log(isa);
+// // const isa =
+// //   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+// // console.log(isa);
 
-const isaNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
-console.log(isaNew);
+// const isaNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+// console.log(isaNew);
 
-console.log(`Just a regular string...`);
+// console.log(`Just a regular string...`);
 
-console.log("String with \n multiple \n lines");
-console.log(`String
-multiple
-lines`);
+// console.log("String with \n multiple \n lines");
+// console.log(`String
+// multiple
+// lines`);
+
+/* Topic: Taking Decisions: if / else Statements */
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log("Sarah can start driving license 🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 1998;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
